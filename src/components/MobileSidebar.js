d70708b408188551dbea7a33c95ebb17a3ffb994
medjed <castleracer@outlook.com>
2023-01-14 17:31:26 -0600

    try other positioning techniques

--- src/components/MobileSidebar.js
+++ src/components/MobileSidebar.js
@@ -5,15 +5,17 @@ export const MobileSidebar = ({ isSidebarOpen }) => {
   return (
     <>
       {isSidebarOpen && (
-        <div className="sidebar-container">
-          <ul className="sidebar-ul">
-            <li className="sidebar-li">Home</li>
-            <li className="sidebar-li">Projects</li>
-            <li className="sidebar-li">About</li>
-          </ul>
-          <div className="sidebar-contact">
-            <div>Call</div>
-            <div>Message</div>
+        <div className="sidebar">
+          <div className="sidebar-container fade-in">
+            <ul className="sidebar-ul">
+              <li className="sidebar-li">Home</li>
+              <li className="sidebar-li">Projects</li>
+              <li className="sidebar-li">About</li>
+            </ul>
+            <div className="sidebar-contact">
+              <div>Call</div>
+              <div>Message</div>
+            </div>
           </div>
         </div>
       )}
