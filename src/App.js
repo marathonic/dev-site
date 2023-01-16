bab097d8ebd0139548e78c68be3aa57cc018d84e
medjed <castleracer@outlook.com>
2023-01-16 11:36:31 -0600

    make nav sticky

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import { useMediaQuery } from "react-responsive";
 import { useState } from "react";
 import { MobileSidebar } from "./components/MobileSidebar";
+import { Projects } from "./components/Projects";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -19,6 +20,7 @@ function App() {
       {/* {isSidebarOpen && <MobileSidebar />} */}
       <header className="App-header"></header>
       <Hero />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/projects.css";
+
+export const Projects = () => {
+  return <div className="projects-container">Projects</div>;
+};
