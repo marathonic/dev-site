ff7c49fadc666da0db082f124b0a069a205e0c2d
medjed <castleracer@outlook.com>
2023-02-05 16:38:43 -0600

    current page link no longer has style with .current-link, try to fix or return to previous commit

--- src/components/MobileSidebar.js
+++ src/components/MobileSidebar.js
@@ -26,7 +26,20 @@ export const MobileSidebar = ({
   });
   // delete this above ~~~
 
-  const closeWithDelay = () => {
+  const handleClick = (e) => {
+    // if same page, return
+    let clicked = e.target;
+    // if (clicked.classList.contains("current-link")) return;
+    let onPage = document.querySelector(".current-link");
+    let onPageText = onPage.textContent;
+    console.log(onPage.textContent);
+    console.log(clicked.id.substring(3));
+    let clickedId = clicked.id.substring(3).toLowerCase();
+    if (clickedId === onPageText) {
+      return;
+    }
+    if (clicked.classList.contains("current-link")) return;
+    clicked.classList.add("side-btn");
     setTimeout(() => {
       setIsSidebarOpen(false);
     }, 700);
@@ -42,9 +55,9 @@ export const MobileSidebar = ({
               <li className="test-border current-link">
                 <div className="test-border-inner current-link">
                   <button
-                    className="sidebar-li current-link side-btn"
+                    className="current-link "
                     id="sb-home"
-                    onClick={closeWithDelay}
+                    onClick={handleClick}
                   >
                     Home
                   </button>
@@ -52,22 +65,14 @@ export const MobileSidebar = ({
               </li>
               <li className="test-border">
                 <div className="test-border-inner">
-                  <button
-                    className="sidebar-li side-btn"
-                    id="sb-proj"
-                    onClick={closeWithDelay}
-                  >
+                  <button className="" id="sb-proj" onClick={handleClick}>
                     Projects
                   </button>
                 </div>
               </li>
               <li className="test-border">
                 <div className="test-border-inner">
-                  <button
-                    className="sidebar-li side-btn"
-                    id="sb-about"
-                    onClick={closeWithDelay}
-                  >
+                  <button className="" id="sb-about" onClick={handleClick}>
                     About
                   </button>
                 </div>
