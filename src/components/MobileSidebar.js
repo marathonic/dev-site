c3b827a63ab77a032a77c4d9c0fcfab7c1b039e6
medjed <castleracer@outlook.com>
2023-02-06 12:17:52 -0600

    home(.current-link) no longer blinks when clicked, the culprit was a cheeky --.side-btn:hover-- style we had missed, that was the thing that was causing it to still be affected by the animation. Next, we need to do two things: X) We want to use the previous style for .current-link, should be easy to copy and paste it from a previous commit. Y) We want to remove the .current-link class from the button that had the classList before, and to give the class to the button we have just clicked now. We have tried e.target.classList.add before, with mixed results. Let's try some more

--- src/components/MobileSidebar.js
+++ src/components/MobileSidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 // import "../yorha.css";
 import "../styles/mobileSidebar.css";
 
@@ -11,27 +11,26 @@ export const MobileSidebar = ({
 
   const [isSelected, setIsSelected] = useState(null);
 
+  function selectLink(e) {
+    // const shit = document.querySelector(".sidebar-ul");
+    // const clickedListItem = e.target;
+    // clickedListItem.classList.add("testing-shit");
+    console.log("clicked this link ==>", e.target.id);
+  }
   // delete this below ~~
-  // const allBtns = document.querySelectorAll(".side-btn");
-  // allBtns?.forEach((btn) => {
-  // btn.addEventListener("click", function (e) {
-  // console.log(e.target.id);
-  // });
-  // });
+  const allBtns = document.querySelectorAll(".side-btn");
+  allBtns?.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      console.log(e.target.id);
+    });
+  });
   // delete this above ~~~
 
   const closeWithDelay = (e) => {
-    // If the button we clicked is disabled, nothing should happen by default, and no code should run, right? That's the point.
-    // So if this is running, it means we've clicked a button other than the disabled one.
-    // SO we should find what that disabled button is, and set it to not be disabled, then we disable the button we have just clicked.
-
-    let btn = e.target;
-
-    let clicked = btn.id.substring(3);
+    let targ = e.target;
+    let clicked = targ.id.substring(3);
     let curr = document.querySelector(".current-link");
     let currentPage = curr.textContent.toLowerCase();
-    console.log("current page", "==>", currentPage);
-    console.log("clicked", "==>", clicked);
     if (currentPage === clicked) return;
     setTimeout(() => {
       setIsSidebarOpen(false);
@@ -39,18 +38,14 @@ export const MobileSidebar = ({
     return;
   };
 
-  useEffect(() => {
-    console.log("test");
-  }, []);
-
   return (
     <>
       {isSidebarOpen && (
         <div className="sidebar">
           <div className="sidebar-container fade-in">
             <ul className="sidebar-ul" id="shit-id-test">
-              <li className="test-border current-link">
-                <div className="test-border-inner current-link">
+              <li className="test-border">
+                <div className="test-border-inner">
                   <button
                     className="sidebar-li current-link side-btn"
                     id="sb-home"
