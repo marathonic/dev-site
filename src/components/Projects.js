3c1dc0e3e3f285543319a432e77a7721e2f7872a
medjed <castleracer@outlook.com>
2023-02-07 16:57:44 -0600

    draft featured Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,26 @@ import React from "react";
 import "../styles/projects.css";
 
 export const Projects = () => {
+  const repos = [
+    { title: "animeLog", img: "src" },
+    { title: "heroFinder", img: "src" },
+    { title: "starShip", img: "src" },
+  ];
+
+  const mappedRepos = repos.map((repo) => {
+    return (
+      <div key={repo.title} className="project-preview">
+        <span>
+          <p>{`${repo.title}`}</p>
+        </span>
+      </div>
+    );
+  });
+
   return (
     <div className="projects-container">
-      <h1 className="h1-projects">featured</h1>
+      <h1 className="h1-projects">Featured</h1>
+      <div className="mapped-repos-container">{mappedRepos}</div>
     </div>
   );
 };
