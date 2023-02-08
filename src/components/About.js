a2f11980836e9c4eb45f7ffdbd76dc386924faa4
medjed <castleracer@outlook.com>
2023-02-08 15:09:02 -0600

    style: give .dev-stack-icons div a display of flex, align icons evenly

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "../styles/about.css";
+import {
+  IoLogoJavascript,
+  IoLogoCss3,
+  IoLogoHtml5,
+  IoLogoReact,
+} from "react-icons/io5";
 
 function About() {
   return (
@@ -8,11 +14,17 @@ function About() {
       <span className="about-description">
         <p>Full stack developer</p>
         <div className="dev-stack-icons">
+          <IoLogoHtml5 size={70} color="crimson" />
+          <IoLogoCss3 size={70} color="purple" />
+          <IoLogoJavascript size={70} color="goldenrod" />
+          <IoLogoReact size={70} color="cyan" />
+          {/* 
           <img
             className="dev-tech-icon"
             src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
             alt="javascript logo"
           />
+           */}
         </div>
       </span>
     </section>
