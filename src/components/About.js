e8c79cc266147710dd34599eb0164c75b4664864
medjed <castleracer@outlook.com>
2023-02-11 12:02:02 -0600

    change About section text

--- src/components/About.js
+++ src/components/About.js
@@ -32,17 +32,17 @@ function About() {
         </p>
         <br />
 
-        <h3 className="about-skills-title">Current Skills</h3>
+        <h3 className="about-skills-title">Experience with</h3>
         <br />
 
         {/* We could just put the <span className="about-description"><div className="dev-stack-icons"><IoLogoHtml5/><etc... /></div></span> right here */}
         {/* And also, put each of those <Icon /> elements inside of individual <span></span> elements, and inside of the spans, add a <p></p> with the name of the tech in the icon. Example: <p>HTML</p> */}
 
-        <ul>
-          <li>JS</li>
-          <li>CSS</li>
-          <li>React</li>
-        </ul>
+        <p>HTML, CSS, JS, React, Firebase</p>
+        <br />
+        <h3 className="about-skills-title">Currently Honing</h3>
+        <br />
+        <p>Node.js</p>
       </span>
     </section>
   );
