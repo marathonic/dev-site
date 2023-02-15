5a4b308f654de436e292244a029cb2764e25f0d6
medjed <castleracer@outlook.com>
2023-02-15 10:43:51 -0600

    add a button and conditionally render timer and instructions

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,10 +10,46 @@ const Hero = ({ currentScroll, setCurrentScroll }) => {
   const [timerSecondRound, setTimerSecondRound] = useState(false);
   const [timerThirdRound, setTimerThirdRound] = useState(false);
   const [timerCountActive, setTimerCountActive] = useState(true);
+  const [isBreatheActive, setIsBreatheActive] = useState(false);
   const [homeRef, homeInView] = useInView({
     triggerOnce: false,
     threshold: 0.8,
   });
+
+  const breathe = () => {
+    setIsBreatheActive(true);
+    // let inhale = document.querySelector(".breathe-inhale");
+    //
+    // let hold = document.querySelector(".breathe-hold");
+    //
+    // let exhale = document.querySelector(".breathe-exhale");
+    //
+    // setTimeout(() => {
+    // setCurrentScroll("home");
+    // }, 100);
+    //
+    // setTimeout(() => {
+    // inhale?.classList.add("hide");
+    //
+    // hold?.classList.remove("hide");
+    // }, 4000);
+    //
+    // setTimeout(() => {
+    // let hold = document.querySelector(".breathe-hold");
+    // hold?.classList.add("hide");
+    //
+    // exhale?.classList.remove("hide");
+    // }, 11000);
+    //
+    // setTimeout(() => {
+    // exhale?.classList.add("hide");
+    // }, 19900);
+    //
+    // setTimeout(() => {
+    // let count = document.querySelector(".timer-count");
+    // count.classList.add("hide");
+    // }, 20000);
+  };
 
   useEffect(() => {
     let inhale = document.querySelector(".breathe-inhale");
@@ -33,7 +69,6 @@ const Hero = ({ currentScroll, setCurrentScroll }) => {
     }, 4000);
 
     setTimeout(() => {
-      // let hold = document.querySelector('.breathe-hold')
       hold?.classList.add("hide");
 
       exhale?.classList.remove("hide");
@@ -47,12 +82,15 @@ const Hero = ({ currentScroll, setCurrentScroll }) => {
       let count = document.querySelector(".timer-count");
       count.classList.add("hide");
     }, 20000);
-  }, []);
+  }, [isBreatheActive]);
 
   useEffect(() => {
     // +++++++++++++++++++++++++++++++++++++ CONTINUE HERE!!!
     // WORKS FOR 3 ROUNDS, yet still keeps going, starting at 8. Find a way to stop at the last cycle, so that the countdown from 8 will stop at 0
     // if(timerThirdRound && timerCount === 1) {}
+
+    if (!isBreatheActive) return;
+
     if (timerThirdRound && timerCount === 0 && timerCountActive) {
       setTimerSecondRound(false);
       setTimerCount(8);
@@ -68,7 +106,7 @@ const Hero = ({ currentScroll, setCurrentScroll }) => {
       setTimerThirdRound(true);
     }
     // timerCountActive && timerCount === 0 && setTimeout(() => setTimerCount )
-  }, [timerCount]);
+  }, [timerCount, isBreatheActive]);
 
   useEffect(() => {
     if (currentScroll === "home") return;
@@ -93,13 +131,22 @@ const Hero = ({ currentScroll, setCurrentScroll }) => {
 
         {/* <img src={songoku} alt="" className="goku-hero" /> */}
         <div className="breathing-circle-container">
-          <div className="breathing-circle"></div>
-          <span className="breathe-text">
-            <p className="breathe-inhale">INHALE</p>
-            <p className="breathe-hold hide">HOLD</p>
-            <p className="breathe-exhale hide">EXHALE</p>
-          </span>
-          <p className="timer-count">{timerCount}</p>
+          <div
+            className={isBreatheActive ? "breathing-circle" : "static-circle"}
+          ></div>
+          {!isBreatheActive && (
+            <button className="breathe-btn" onClick={breathe}>
+              Breathe
+            </button>
+          )}
+          {isBreatheActive && (
+            <span className="breathe-text">
+              <p className="breathe-inhale">INHALE</p>
+              <p className="breathe-hold hide">HOLD</p>
+              <p className="breathe-exhale hide">EXHALE</p>
+            </span>
+          )}
+          <p className="timer-count">{isBreatheActive && timerCount}</p>
         </div>
         {/* {`${Goku}`} */}
         {/* <img
